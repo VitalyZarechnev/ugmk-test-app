1d7c1f6b278447c0299cd43bfeac8d88f0396f25
VitalyZarechnev <v.zarechnev@redmadrobot.com>
2023-05-12 16:11:27 +0700

    Fix const name

--- src/modules/products/presentation/details/helper.ts
+++ src/modules/products/presentation/details/helper.ts
@@ -4,7 +4,7 @@ import { IDetailsChartData } from "./interfaces/IDetailsChartData";
 import { formatWeight } from "@/utils/format";
 
 export const getDetailsChartData = (data: ProductModel[], factoryId: number, monthId: number): IDetailsChartData => {
-	const productsSumm = data.reduce((acc, product) => {
+	const productsSum = data.reduce((acc, product) => {
 		acc.product1 += product.product1;
 		acc.product2 += product.product2;
 		acc.product3 += product.product3;
@@ -19,8 +19,8 @@ export const getDetailsChartData = (data: ProductModel[], factoryId: number, mon
 	return {
 		factoryId,
 		month: MONTHS[monthId - 1],
-		product1: formatWeight(productsSumm.product1),
-		product2: formatWeight(productsSumm.product2),
-		product3: formatWeight(productsSumm.product3),
+		product1: formatWeight(productsSum.product1),
+		product2: formatWeight(productsSum.product2),
+		product3: formatWeight(productsSum.product3),
 	};
 };
